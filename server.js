e3169c2674f62772d28437c8093c54cbbffaa9ee
Aryan <Aryan1284@gmail.com>
2025-05-25 14:11:59 +0530

    Beta

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://authenticate-w6po.onrender.com', credentials: true }));
 app.use(cookieParser())
 connectDB();
 
